627852660f80b1a75873a32766548ace0da98992
Antoine Dujardin <antoinedujardin42@gmail.com>
2020-08-10 15:07:19 +0200

    add raspberry pi setup.sh

---
+++ conf/raspberry-pi/setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+echo "deb https://repos.influxdata.com/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=arm64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update && sudo apt upgrade
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties telegraf git zsh docker-ce docker-ce-cli containerd.io
+
+sudo adduser antoine
+sudo usermod -aG sudo antoine
+sudo su antoine
+sudo userdel pi
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo usermod -aG docker $USER
