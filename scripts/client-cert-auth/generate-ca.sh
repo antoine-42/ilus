3f18a70f86c8ee97a785d37333197c06fb8ec87c
Antoine Dujardin <antoinedujardin42@gmail.com>
2020-08-22 20:26:55 +0200

    make keys better for client/server cert auth

--- scripts/client-cert-auth/generate-ca.sh
+++ scripts/client-cert-auth/generate-ca.sh
@@ -3,4 +3,5 @@
 echo "Creating the certificate authority."
 openssl genrsa -des3 -out ca.key 4096
 echo "Creating the certificate authority certificate, valid for 10 years."
-openssl req -new -x509 -days 3650 -key ca.key -out ca.crt
+echo extendedKeyUsage = serverAuth >> extfile.cnf
+openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -extfile extfile.cnf
--- scripts/client-cert-auth/generate-client-certificate.sh
+++ scripts/client-cert-auth/generate-client-certificate.sh
@@ -10,6 +10,7 @@ openssl genrsa -des3 -out $1.key 4096
 echo "Creating user certificate signing request."
 openssl req -new -key $1.key -out $1.csr
 echo "Signing the csr to a certificate, valid for 10 years."
-openssl x509 -req -days 3650 -in $1.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out $1.crt
+echo extendedKeyUsage = clientAuth > extfile-client.cnf
+openssl x509 -req -days 3650 -in $1.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out $1.crt -extfile extfile.cnf
 echo "Creating pfx package."
 openssl pkcs12 -export -out $1.pfx -inkey $1.key -in $1.crt -certfile ca.crt
