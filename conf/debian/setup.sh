eec6ba714b6641824019b613642c0ecf6a053969
Antoine Dujardin <antoinedujardin42@gmail.com>
2020-08-23 23:00:24 +0200

    update proxmox setup: mount mdadm volume to vm

--- conf/debian/setup.sh
+++ conf/debian/setup.sh
@@ -12,7 +12,7 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo apt update && sudo apt upgrade
-sudo apt install curl git zsh htop smartmontools lm-sensors    apt-transport-https ca-certificates gnupg-agent software-properties-common    docker-ce docker-ce-cli containerd.io
+sudo apt install curl git zsh htop rsync smartmontools lm-sensors    apt-transport-https ca-certificates gnupg-agent software-properties-common    docker-ce docker-ce-cli containerd.io
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo usermod -aG docker $USER
@@ -20,3 +20,8 @@ sudo usermod -aG docker $USER
 mkdir git
 cd git
 git clone --recurse-submodules -j8 git@github.com:antoine-42/projects.git
+lsblk -f
+# copy the volume uuid
+sudo nano /etc/fstab
+# add
+# UUID=<volume uuid>       /mnt/storage    ext4 defaults,auto      0       2
--- conf/proxomox/setup.sh
+++ conf/proxomox/setup.sh
@@ -4,14 +4,16 @@ sudo usermod -aG sudo antoine
 sudo su antoine
 
 sudo apt update && sudo apt upgrade
-sudo apt install curl git zsh htop    mdadm
+sudo apt install curl git zsh htop rsync    mdadm
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # reboot
 
 sudo mkdir /mnt/storage
 lsblk -f
-# copy the volume id
+# copy the volume uuid
 sudo nano /etc/fstab
 # add:
-# UUID=<volume id>       /mnt/storage    ext4    defaults,auto,nofail    0       2
+# UUID=<volume uuid>       /mnt/storage    ext4    defaults,auto,nofail    0       2
+# https://pve.proxmox.com/wiki/Physical_disk_to_kvm
+sudo qm set <VM ID> -scsi<+1> /dev/disk/by-uuid/<volume uuid>
