78d6bad886a2202dfe9aa04a1fe06d3d33635ad6
Antoine Dujardin <antoinedujardin42@gmail.com>
2020-12-25 22:48:55 +0100

    fixes

--- conf/raspberry-pi/kiosk/kiosk.sh
+++ conf/raspberry-pi/kiosk/kiosk.sh
@@ -5,4 +5,3 @@ xset s noblank # don't blank the video device
 matchbox-window-manager -use_titlebar no &
 unclutter &    # hide X mouse cursor unless mouse activated
 grafana-kiosk -c /home/antoine/git/projects/ilus/conf/raspberry-pi/kiosk/grafana-kiosk.yaml
-#chromium-browser --display="$DISPLAY" --kiosk --incognito --window-position=0,0 "https://grafana.ilus.pw/playlists/play/1?kiosk&autofitpanels"
--- conf/raspberry-pi/kiosk/setup.sh
+++ conf/raspberry-pi/kiosk/setup.sh
@@ -2,7 +2,8 @@
 
 sudo apt install xserver-xorg xinit x11-xserver-utils lightdm    chromium-browser matchbox-window-manager xautomation unclutter upower libgles2
 
-sudo cp kiosk.service /usr/lib/systemd/user/
+sudo cp kiosk.service /lib/systemd/system/
+sudo systemctl daemon-reload
 sudo systemctl enable kiosk && sudo systemctl start kiosk
 
 # Import certificate to chrome
@@ -13,6 +14,11 @@ sudo mkdir /etc/chromium-browser/policies/managed
 echo "{\"AutoSelectCertificateForUrls\": [\"{\\\"pattern\\\":\\\"*\\\",\\\"filter\\\":{}}\"]}" | sudo tee /etc/chromium-browser/policies/managed/AutoSelectCertificateForUrls.json
 
 # Install grafana-kiosk
-wget https://github.com/grafana/grafana-kiosk/releases/download/v1.0.2/grafana-kiosk.linux.arm64 -O /tmp/grafana-kiosk.linux.armv7
+wget https://github.com/grafana/grafana-kiosk/releases/download/v1.0.2/grafana-kiosk.linux.armv7 -O /tmp/grafana-kiosk.linux.armv7
 sudo cp /tmp/grafana-kiosk.linux.armv7 /usr/bin/grafana-kiosk
 sudo chmod 755 /usr/bin/grafana-kiosk
+
+# Fill in grafana-kiosk login info
+cp /home/antoine/git/projects/ilus/conf/raspberry-pi/kiosk/template-grafana-kiosk.yml /home/antoine/git/projects/ilus/conf/raspberry-pi/kiosk/grafana-kiosk.yaml
+chmod 600 /home/antoine/git/projects/ilus/conf/raspberry-pi/kiosk/grafana-kiosk.yaml
+nano /home/antoine/git/projects/ilus/conf/raspberry-pi/kiosk/grafana-kiosk.yaml
